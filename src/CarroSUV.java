382edb23b887dafbb35e7fc2a73348746b407dcc
Thiago <thiagosales46@gmail.com>
2023-11-17 15:16:27 -0300

    "funcionando 1.0"

--- src/CarroSUV.java
+++ src/CarroSUV.java
@@ -1,6 +1,7 @@
 public class CarroSUV extends Veiculo {
     public CarroSUV(String marca, String modelo, int ano, double diaria) {
         super(marca, modelo, ano, diaria);
+        setDiaria(diaria);
     }
 
     @Override
--- src/CarroSedan.java
+++ src/CarroSedan.java
@@ -1,9 +1,8 @@
 public class CarroSedan extends Veiculo {
     public CarroSedan(String marca, String modelo, int ano, double diaria) {
         super(marca, modelo, ano, diaria);
-        System.out.println(diaria);
         setDiaria(diaria);
-        System.out.println(diaria);
+
     }
 
     @Override
